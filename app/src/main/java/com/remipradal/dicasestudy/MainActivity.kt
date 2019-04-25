9a1401239a19f637a2a5dfc55250d1668b550fa0
Remi Pradal <remi.pradal@gmail.com>
2019-04-25 15:18:54 +0200

    First commit

---
+++ app/src/main/java/com/remipradal/dicasestudy/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.remipradal.dicasestudy
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
